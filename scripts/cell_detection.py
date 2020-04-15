f1693e524f29d226b1d65b007bd07c30f7dcbc3c
michael-yang2 <michael-yang2@github.com>
2020-04-15 11:56:35 -0700

    added barebones skimage blob detection

--- scripts/cell_detection.py
+++ scripts/cell_detection.py
@@ -4,6 +4,7 @@ from matplotlib import collections as mc
 from PIL import Image,ImageOps,ImageEnhance
 from skimage.exposure import equalize_adapthist
 from skimage.io import imread, imsave
+import skimage
 import mahotas as mh
 import cv2
 import numpy as np
@@ -12,18 +13,21 @@ import argparse
 
 def run(img_file, write_img, type): 
 	preprocessed_img = image_preprocessing(img_file,type)
+	blob_detection(preprocessed_img)
+
 def image_preprocessing(img_file, type): #filtering, blurring: preprocessing for OpenCV's blob detection
 	img = Image.open(img_file)
 	img = np.asarray(img)
 	img = cv2.GaussianBlur(img,(5,5),0)
-	img = equalize_adapthist(img,clip_limit = 0.01)
+	img = equalize_adapthist(img,clip_limit = 0.01) #doesn't seem to do much
 	plt.imshow(img)
 	plt.show()
 	r,g,b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
 	#RGBA image
 	img_threshold = np.max((np.percentile(r, 60),np.percentile(g, 60),np.percentile(b, 60))) #60th percentile arbitrarily chosen: only pixels brighter than 60th percentile remain
-	low_values_flag = img < img_threshold
-	img[low_values_flag] = 0
+	img[img < img_threshold] = 0
+	# img = (img*255).astype(np.uint8)
+	# img = cv2.Canny(img,100,100)
 	plt.imshow(img)
 	plt.show()
 	return img
@@ -37,8 +41,8 @@ def image_preprocessing(img_file, type): #filtering, blurring: preprocessing for
 	# b = np.expand_dims(b, axis=2)
 	# a = np.expand_dims(a, axis=2)
 	# img = np.concatenate((r, g, b, a), axis=2)
-def blob_detection(preprocessed_img): #TBD
-	pass
+def blob_detection(preprocessed_img): #returns 2d array of coordinates
+	return skimage.feature.blob_log(preprocessed_img)
 def write_to_file(write_img, write_file):#write_img is a numpy array, write_file is a filename
 	Image.fromarray(write_img).save(write_file)
 def main():
